2dbea563d16e6281d86e23905f03a8028c38f272
rlozib <87642220+rlozib@users.noreply.github.com>
2021-09-02 17:38:08 +0900

    21.09.02 오후 작업물

--- refer-effect/assets/js/custom.js
+++ refer-effect/assets/js/custom.js
@@ -6,6 +6,45 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 
 
+//Code tab menu
+
+//js
+const viewTitle = document.querySelectorAll(".view-title ul li");
+const viewCont = document.querySelectorAll(".view-cont > div");
+
+viewTitle.forEach((element, index) => {
+    element.addEventListener("click", function(){
+        
+        viewTitle.forEach( el => {
+            el.classList.remove("active");
+        });
+        element.classList.add("active");
+
+        viewCont.forEach( el => {
+            el.style.display = "none";
+        })
+        viewCont[index].style.display = "block";
+    });
+})
+
+//jquery
+// const viewTitle = $(".view-title ul li");     
+// const viewCont = $(".view-cont > div"); 
+
+
+// viewTitle.click(function(){
+//     let target = $(this);
+//     viewTitle.removeClass("active");
+//     target.addClass("active");
+
+//     let index = target.index();
+//     viewCont.css("display","none");
+//     viewCont.eq(index).css("display","block");
+// });
+
+
+
+
 //Modal
 document.querySelector(".info button").addEventListener("click", function(){
     document.querySelector("#modal").classList.add("show");
@@ -15,3 +54,15 @@ document.querySelector(".info button").addEventListener("click", function(){
 document.querySelector(".modal-cont button").addEventListener("click", function(){
     document.querySelector("#modal").classList.add("hide");
 });
+
+
+
+
+
+// $(".info button").click(function(){
+//     $("#modal").removeClass().addClass("show")
+// });
+
+// $(".modal-cont button").click(function(){
+//     $("#modal").addClass("hide")							  
+// });
