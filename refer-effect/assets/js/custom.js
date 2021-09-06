70c5629bef3551f8f8dea156cc2996ff4015fe3b
cherin0115 <87642220+rlozib@users.noreply.github.com>
2021-09-06 13:20:46 +0900

    21.09.06 오전 작업물

--- refer-effect/assets/js/custom.js
+++ refer-effect/assets/js/custom.js
@@ -102,16 +102,16 @@ viewTitle.forEach((element, index) => {
 
 
 //Modal
-document.querySelector(".info button").addEventListener("click", function(){
-    document.querySelector("#modal").classList.add("show");
-    document.querySelector("#modal").classList.remove("hide");
-});
-
-document.querySelector(".modal-cont button").addEventListener("click", function(){
-    document.querySelector("#modal").classList.add("hide");
-});
-
-
+function modal() {
+    document.querySelector(".info button").addEventListener("click", function(){
+        document.querySelector("#modal").classList.add("show");
+        document.querySelector("#modal").classList.remove("hide");
+    });
+    
+    document.querySelector(".modal-cont button").addEventListener("click", function(){
+        document.querySelector("#modal").classList.add("hide");
+    });
+}
 
 
 
